6a9039e9ad2ae6404e595009e0cfb83172b2cf59
vits56 <bastosduartebruno@outlook.com>
2020-10-16 03:18:39 -0300

    Update page-orphanages.js

--- public/scripts/page-orphanages.js
+++ public/scripts/page-orphanages.js
@@ -1,22 +1,34 @@
-const map = L.map('mapid').setView([-27.2206781,-49.6535601], 16);
+// creat map
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+const map = L.map('mapid').setView([-27.2206781,-49.6535601], 15);
 
 
-L.marker([-27.2206781,-49.6535601], {icon})
-.addTo(map)
-.bindPopup(popup)
+//creat tilelayer
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png')
+.addTo(map);
+
+
+// creat icon
 
 const icon = l.icon({
-    iconurl: "./public/images/map-marker.svg",
+    iconUrl: "./public/images/map-marker.svg",
     iconSize: [58, 68],
     iconAnchor: [29, 68],
     popupAnchor: [170, 2]
 })
+// create popup overlay
 
 const popup = l.popup({
     closeButton: false,
     className: 'map-popup',
     minWidth: 240,
     minHeight: 240
-}).setContent('Lar das meninas <a href="orphanage.html?id=1" class="choose-orphanage"> <img src="./public/images/arrow-white.svg" </a>')
+}).setContent('Lar das meninas <a href="orphanage.html?id=1" class="choose-orphanage"> <img src="./public/images/arrow-white.svg"> </a>')
+
+// creat and add maker
+
+L
+.marker([-27.2206781,-49.6535601], {icon})
+.addTo(map)
+.bindPopup(popup)
