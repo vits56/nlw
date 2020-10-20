664ac5c0b19a7d5b46899539173e2c45fc1b2382
vits56 <bastosduartebruno@outlook.com>
2020-10-20 10:01:17 -0300

    orphanage-details

--- public/scripts/page-orphanage.js
+++ public/scripts/page-orphanage.js
@@ -1,6 +1,15 @@
+const options = {
+    dragging: false,
+    touchZoom: false,
+    doubleClickZoom: false,
+    scrollWheelZoom: false,
+    ZoomControl: false
+
+}
+
 // creat map
 
-const map = L.map('mapid').setView([-27.2206781,-49.6535601], 15);
+const map = L.map('mapid', options).setView([-27.2206781,-49.6535601], 15);
 
 
 //creat tilelayer
@@ -17,18 +26,10 @@ const icon = l.icon({
     iconAnchor: [29, 68],
     popupAnchor: [170, 2]
 })
-// create popup overlay
 
-const popup = l.popup({
-    closeButton: false,
-    className: 'map-popup',
-    minWidth: 240,
-    minHeight: 240
-}).setContent('Lar das meninas <a href="orphanage.html?id=1" class="choose-orphanage"> <img src="./public/images/arrow-white.svg"> </a>')
 
 // creat and add maker
 
 L
 .marker([-27.2206781,-49.6535601], {icon})
 .addTo(map)
-.bindPopup(popup)
