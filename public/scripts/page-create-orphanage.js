cf8c37577cd546c7a40e8c64ae55e98c44f512d2
vits56 <bastosduartebruno@outlook.com>
2020-10-29 03:01:19 -0300

    map

---
+++ public/scripts/page-create-orphanage.js
@@ -0,0 +1,21 @@
+const map = L.map("mapid").setView([-27.2109325, -49.6448719], 15);
+
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
+
+const icon = L.icon({
+  iconUrl: "./public/images/map-marker.svg",
+  iconSize: [58, 68],
+  iconAnchor: [29, 68]
+})
+
+const popup = l.popup({
+  closeButton: false,
+  className: 'map-popup',
+  minWidth: 240,
+  minHeigth: 240
+}).setContent('Lar das meninas <a href="/orphanage?id=${id}"> <img src="/images/arrow-white.svg"> </a>`')
+
+L
+.marker([-27.222633, -49.6455874], { icon })
+.addTo(map)
+.bindPopup(popup)
--- public/scripts/page-orphanages.js
+++ public/scripts/page-orphanages.js
@@ -1,36 +1,22 @@
-const map = L.map("mapid").setView([-27.2109325, -49.6448719], 15);
+const map = L.map("mapid").setView([-27.2109325, -49.6448719], 15)
 
-L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map)
 
 const icon = L.icon({
-  iconUrl: "/images/map-marker.svg",
+  iconUrl: "./public/images/map-marker.svg",
   iconSize: [58, 68],
   iconAnchor: [29, 68],
-  popupAnchor: [170, 2],
-});
+  popupAnchor: [170, 2]
+})
 
-function addMarker({ id, name, lat, lng } = orphanage) {
-  const popup = L.popup({
-    closeButton: false,
-    className: "map-popup",
-    minWidth: 240,
-    minHeight: 240,
-  }).setContent(
-    `${name} <a href="/orphanage?id=${id}"> <img src="/images/arrow-white.svg"> </a>`
-  );
+const popup = L.popup({
+  closeButton: false,
+  className: 'map-popup',
+  minWidth: 240,
+  minHeigth: 240
+}).setContent('Lar das meninas <a href="/orphanage?id=${id}"> <img src="/images/arrow-white.svg"> </a>`')
 
-  L.marker([lat, lng], { icon }).addTo(map).bindPopup(popup);
-}
-
-const orphanagesSpan = document.querySelectorAll(".orphanages span");
-
-orphanagesSpan.forEach((span) => {
-  const orphanage = {
-    id: span.dataset.id,
-    name: span.dataset.name,
-    lat: span.dataset.lat,
-    lng: span.dataset.lng,
-  };
-
-  addMarker(orphanage);
-});
+L
+.marker([-27.222633, -49.6455874], { icon })
+.addTo(map)
+.bindPopup(popup)
